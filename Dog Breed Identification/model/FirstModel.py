6b49398ea50f153bc9e4b098086e9b0aa7d70450
Leezed525 <leezed525@qq.com>
2023-10-16 13:15:01 +0800

    增加通用模块

--- Dog Breed Identification/model/FirstModel.py
+++ Dog Breed Identification/model/FirstModel.py
@@ -1,25 +1,34 @@
 from torch import nn
+from .common.Conv import Conv
+from .common.BottleNeck import BottleNeck
 
 
 class FirstModel(nn.Module):
-    def __init__(self):
+    def __init__(self, is_debug=False):
         super(FirstModel, self).__init__()
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 32, 3, 1, 1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(inplace=True)
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(32, 64, 3, 1, 1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True)
-        )
-        self.out = nn.Linear(64 * 64 * 64, 120)
+        self.is_debug = is_debug
+        self.conv1 = Conv(3, 16, 3, 1)
+        self.conv2 = Conv(16, 32, 3, 1)
+        # 降低图像大小
+        self.pool1 = nn.MaxPool2d(2, 2)
+        # 通过卷积缩小图像大小
+        self.conv3 = BottleNeck(32, 32, shortcut=True)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.conv4 = BottleNeck(32, 32, shortcut=True)
+        self.pool3 = nn.MaxPool2d(2, 2)
+        self.conv5 = Conv(32, 16, 7, 5, 0)
+        self.out = nn.Linear(16 * 6 * 6, 120)
         nn.init.kaiming_normal_(self.out.weight)
 
     def forward(self, x):
-        x=self.conv1(x)
-        x=self.conv2(x)
-        x=x.view(x.size(0),-1)
-        x=self.out(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.pool1(x)
+        x = self.conv3(x)
+        x = self.pool2(x)
+        x = self.conv4(x)
+        x = self.pool3(x)
+        x = self.conv5(x)
+        x = x.view(x.size(0), -1)
+        x = self.out(x)
         return x
